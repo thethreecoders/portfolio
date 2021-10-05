3a4e1d43099b8499cdac124bc93ecaf79fb4499f
thethreecoders <91870056+thethreecoders@users.noreply.github.com>
2021-10-05 08:13:06 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 console.log('hello!');
+function hideshow(){
+    let x=document.getElementsByClassName('menu');
+    
+    if(x[0].style.display!='none')
+        {
+            x[0].style.display='none'
+            //x[0].className='rp'; 
+        }
+        else
+        {
+            x[0].style.display='inline';
+        }
+}    
